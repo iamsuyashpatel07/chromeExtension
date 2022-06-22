7d2e22c6d7a9693d60e47f9a8d987a144f718b23
[suyash patel] <suyashpatelmrj@gmail.com>
2022-06-22 10:32:13 +0530

    bookmark function in console done

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,53 @@
 (() => {
     let youtubeLeftControls, youtubePlayer;
     let currentVideo = "";
+    let currentVideoBookmarks = [];
     //accessing Youtube player,accessing control
-    chrome.runtime.onMessage.addListner((obj, sender, response) => {
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
         //destructuring the value
-        const { type, value, viseoId } = obj;
-        if (type === " NEW") {
-            currentVideo = viseoId;
+        const { type, value, videoId } = obj;
+        if (type === "NEW") {
+            currentVideo = videoId;
             newVideoLoaded();
 
         }
 
-    })
+    });
+    const newVideoLoaded = () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+
+        if (!bookmarkBtnExists) {
+            const bookmarkBtn = document.createElement("img");
+            console.log(bookmarkBtnExists);
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+            youtubeLeftControls.append(bookmarkBtn);
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+
+        }
+    }
+    const addNewBookmarkEventHandler = () => {
+        const currentTime = youtubePlayer.currentTime;
+        const newBookmark = {
+            time: currentTime,
+            desc: "Bookmark at" + getTime(currentTime),
+        };
+        console.log(newBookmark);
+        chrome.storage.sync.set({
+
+            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+
+        });
+    }
+    newVideoLoaded();
 })();
+const getTime = t => {
+    var date = new Date(0);
+    date.setSeconds(1);
+
+    return date.toISOString().substr(11, 0);
+}
